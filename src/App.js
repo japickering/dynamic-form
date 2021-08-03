04f07bf32a4724d60bc6a70618dc926f32480d08
James Pickering <james.pickering@gmail.com>
2021-08-03 11:00:25 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import data from "./data.json";
+import "./styles.css";
+//console.log(data);
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: data
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="form-container">
+          <form className="form">
+            <h3>{this.state.data.questions[0].title}</h3>
+            <ul>
+              {this.state.data.questions[0].fields.map((item) => {
+                return (
+                  <li key={item.label}>
+                    <input
+                      type="text"
+                      name={item.name}
+                      placeholder={item.label}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+            <h3>{this.state.data.questions[1].title}</h3>
+            <ul>
+              {this.state.data.questions[1].fields.map((item) => {
+                if (item.type === "text") {
+                  return (
+                    <li key={item.name}>
+                      <input
+                        type="text"
+                        name={item.name}
+                        placeholder={item.label}
+                      />
+                    </li>
+                  );
+                } else {
+                  return (
+                    <select name={item.name}>
+                      <option>Select..</option>
+                      {item.options.map((opt) => {
+                        return (
+                          <option key={opt} value={opt}>
+                            {opt}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  );
+                }
+              })}
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
